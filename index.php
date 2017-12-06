f9e6acb7402455fb8489d4f1bbbf0f9152a55eed
goadelaja2002 <goadelaja2002@gmail.com>
2017-12-07 00:49:00 +0100

    main page completed and styled

--- index.php
+++ index.php
@@ -3,16 +3,29 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome to my world</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
   </head>
   <body>
     <header>
-
+      <div id = "top_half" class="top_half">
+        <h1 class="wrapper">Gbenga Adelaja</h1>
+        <p class="min">Made In Nigeria</p>
+        <h3 class="wrapper">Full Stack Developer</h3>
+        <h6 class="wrapper">php Java Python Html Css Javascript</h6>
+      </div>
+      <nav id="mynavs">
+        <ul>
+          <li>Home</li>
+          <li>Portfolio</li>
+          <li>About</li>
+        </ul>
+      </nav>
     </header>
     <section>
-
     </section>
     <footer>
-        &copy copyright <?php echo Date('Y') ?>
+      <!--  &copy copyright <?php echo Date('Y') ?>-->
     </footer>
 
   </body>
