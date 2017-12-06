db284463559e6f2f6d1fc1670a7f3225672a559d
goadelaja2002 <goadelaja2002@gmail.com>
2017-12-06 12:40:14 +0100

    Structure layout of the Website

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to my world</title>
+  </head>
+  <body>
+    <header>
+
+    </header>
+    <section>
+
+    </section>
+    <footer>
+        &copy copyright <?php echo Date('Y') ?>
+    </footer>
+
+  </body>
+</html>
