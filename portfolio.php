e2e7244989286aa4f16d1fbd386f0ea2891fb76a
goadelaja2002 <goadelaja2002@gmail.com>
2017-12-17 01:36:22 +0100

    added portfolio and heritageplace site

---
+++ portfolio.php
@@ -0,0 +1,30 @@
+<?php
+$PageTitle = "My Works";
+include("inc/header.php");
+
+?>
+
+<header>
+  <div id = "top_half" class="top_half">
+    <h1 class="wrapper">Gbenga Adelaja</h1>
+
+  </div>
+  <nav id="mynavs">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About</a></li>
+
+    </ul>
+  </nav>
+  </header>
+  <section>
+    <div class="main">
+      <nav>
+        <ul>
+          <li><a href="heritage/index.php">Heritage Place</a></li>
+        </ul>
+      </nav>
+
+    </div>
+
+  </section>
